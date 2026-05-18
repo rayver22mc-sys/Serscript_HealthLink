f62cd80ab1b100f753af532fcd404c2f966aa17a
Houdini <beatrizmaeyoungku08@gmail.com>
2026-05-18 14:23:42 +0800

    fixed CoursePage UI file path

--- src/PageSections/coursePage.php
+++ src/PageSections/coursePage.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HealthLink: Courses</title>
-    <link rel="stylesheet" href="coursePages.css">
+    <link rel="stylesheet" href="../CoursePage/coursePages.css">
 </head>
 <body>
     <header>
